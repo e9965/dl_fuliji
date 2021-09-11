baa8c523b207f066965d7697c91fcbfd098c7a3f
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-09-12 01:03:10 +0800

    Fix Bug

--- set.sh
+++ set.sh
@@ -1,6 +1,6 @@
 echo "正在更新&安装"
 curl -so pic_dl.sh https://raw.githubusercontent.com/e9965/dl_fuliji/main/pic_dl.sh
 curl -so sub_dl.sh https://raw.githubusercontent.com/e9965/dl_fuliji/main/sub_dl.sh
-curl -so Header https://raw.githubusercontent.com/e9965/dl_fuliji/main/Header.sh
+curl -so Header https://raw.githubusercontent.com/e9965/dl_fuliji/main/Header
 curl -so dl.sh https://raw.githubusercontent.com/e9965/dl_fuliji/main/dl.sh
 chmod +rwx *.sh && ./dl.sh
