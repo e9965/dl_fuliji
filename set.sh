ee29b2d2883784796a86eb5e2fe4484da61885bd
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-09-12 12:51:48 +0800

    Fix Bug for 一鍵安裝脚本

--- set.sh
+++ set.sh
@@ -1,4 +1,6 @@
 echo "正在更新&安装"
 for i in pic_dl.sh sub_dl.sh Header.template dl.sh
-curl -so ${i} "https://raw.githubusercontent.com/e9965/dl_fuliji/main/${i}"
+do
+    curl -so ${i} "https://raw.githubusercontent.com/e9965/dl_fuliji/main/${i}"
+done
 chmod +rwx *.sh && ./dl.sh
