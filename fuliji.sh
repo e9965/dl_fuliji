73a4a92ecb2be6c47bca24b63cd807a365dd3d41
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-12-09 15:23:32 +0800

    Fix Bug & Update

---
+++ fuliji.sh
@@ -0,0 +1,59 @@
+export baseURL_fuliji="https://www.112w.cc/"
+function readFuliji(){
+    arrayF=($(cat Msg.tmp | grep -oE "text[^]]+" | grep -E "url|http" | grep -oE 'http[^"]+'))
+#Check the Which URL is valid
+    for i in ${arrayF[@]}
+    do
+        [[ ${i} =~ ${baseURL_fuliji} ]] && echo "We recieve the [${i}] URL" && echo ${i} >> Msg_File_Fuliji.tmp
+    done
+}
+
+function createFuliji(){
+    [[ ! -f Msg_File_Fuliji.tmp ]] && touch Msg_File_Fuliji.tmp
+    >& Msg_File_Fuliji.tmp
+    >& downList.fuliji.html.tmp
+}
+
+function checkUniq_Fuliji(){
+    cp Msg_File_Fuliji.tmp Msg_File_Fuliji_uniq.tmp
+    >& Msg_File_Fuliji.tmp
+    sort -V Msg_File_Fuliji_uniq.tmp | uniq | sed '/^$/d' >> Msg_File_Fuliji.tmp
+    rm -rf Msg_File_Fuliji_uniq.tmp
+}
+
+function downFuliji(){
+    checkUniq_Fuliji
+    for i in $(cat Msg_File_Fuliji.tmp)
+    do
+        >& downList.fuliji.html.tmp
+        #Clear The Previous Pic Url
+        fuliji_Code=$(echo ${i}|sed -E "s@${baseURL_fuliji}m?|(p[[:digit:]]+)?\.aspx@@g")
+        storePicURL_Fuliji ${fuliji_Code} 1
+        pageNum=$(grep '<table align="center">' fuliji.html.tmp | grep -oE "<a[^<]+" | grep -oE ">[[:digit:]]" | cut -d'>' -f2 | tail -1)
+        title_Fuliji=$(grep -oE '<h1[^<]+' fuliji.html.tmp| sed -E "s@ |<h1>@@g")
+        backRespond "start to download File [${title_Fuliji}.pdf]"
+        for ((j=2;${j}<${pageNum};j++))
+        do
+            storePicURL_Fuliji ${fuliji_Code} ${j}
+        done
+        cat downList.fuliji.html.tmp | xargs -P 5 -I X wget ${baseURL_fuliji}X -P downDir/Fuliji --header='referer: https://www.112w.cc/mn86866c49p5.aspx'
+        #cat downList.fuliji.html.tmp | xargs -P 10 -I X echo "${baseURL_fuliji}X" -P downDir/Fuliji
+        wait
+        convert $(find downDir/Fuliji | grep "downDir/Fuliji/") downDir/Fuliji/${title_Fuliji}.pdf
+        remove_Pic_Raw
+        backRespond "File [${title_Fuliji}.pdf] is downloaded"
+        echo "File [${title_Fuliji}.pdf] is downloaded"
+    done
+}
+
+function storePicURL_Fuliji(){
+    curl "${baseURL_fuliji}m${1}p${2}.aspx" > fuliji.html.tmp
+    grep '<div style="text-align: center;">' fuliji.html.tmp | grep -oE '<img[^.]+.jpg' | cut -d'"' -f2 >> downList.fuliji.html.tmp
+}
+
+function remove_Pic_Raw(){
+    for k in $(ls downDir/Fuliji|grep -v '\.pdf')
+    do
+        rm -rf downDir/Fuliji/${k}
+    done
+}
--- telegram.sh
+++ telegram.sh
@@ -1,8 +1,42 @@
-#Version:2021-12-4[17:21]
+#Version:2021-12-5[10:10]
 export oldOffset=""
+########################################################################################################
+#Extended Function Regions
+source fuliji.sh
+#fuliji.sh is the shell script to download the photo from 112w.cc
+########################################################################################################
 function NewOffset(){
 #$(NewOffset) return the offset for getUpdates method. The offset is the latest msg's update_id plus 1
     echo $(($(Tmethod getUpdates | grep -oE "update_id[^,]+" | sort -V | tail -1 | cut -d'"' -f2 | grep -oE "[[:digit:]]+") + 1))
+}
+
+function readSrc_Ex(){
+    readFuliji
+}
+
+function downFile_Ex(){
+    [[ ${flagArray[1]} = 1 ]] && downFuliji
+}
+
+function createFolder_Ex(){
+    [[ ${flagArray[1]} == 1 ]] && mkdir -p downDir/Fuliji
+}
+
+function checkRequest(){
+##When the FlagArray value = 0 > The File is Empty
+##When the FlagArray value = 1 > The File is not empty
+##For the overall function, it returns 0 whenever anyone of the file is not empty
+    flagArray[0]=$(checkEmpty "Msg_File.tmp")
+    flagArray[1]=$(checkEmpty "Msg_File_Fuliji.tmp")
+    [[ `expr $(echo ${flagArray[@]} | sed 's@ @ + @g')` > 0 ]] && return 0 || return 1
+}
+
+function createMsgFile_Ex(){
+    [[ ${flagArray[1]} == 1 ]] && createFuliji
+}
+
+function displayLog_Ex(){
+    displayLog "fuliji_work" "Msg_File_Fuliji.tmp"
 }
 
 function noMsg(){
@@ -12,15 +46,13 @@ function noMsg(){
 }
 
 function main(){
-    clear
     while true
     do
-        echo "Telegram Bot Service is running"
+        echo -ne "Telegram Bot Service is running\r"
         [[ $(noMsg) == 1 ]] && work
-        sleep 2s && clear
+        sleep 2s
     done
 }
-
 
 function saveMsg(){
     saveMsg_offset=${1}
@@ -41,31 +73,41 @@ function displayLog(){
 }
 
 function work(){
-    sleep 2s
+    sleep 1s
     endFlag=0
     creatMsgFile
+    createMsgFile_Ex
     echo "We get the request and start to work"
     saveMsg ${oldOffset}
     displayLog "plain json request" "Msg.tmp"
     export chatId=$(cat Msg.tmp | grep -oE "chat[^,]+" | head -1 | grep -oE "[[:digit:]]+")
-    backRespond 'Bot starts to collect ur request'
+    backRespond 'Bot gets your call ~ Send the cmd </end> to start the process.'
     while [[ ${endFlag} == 0 ]]
     do
         readSrc
+        readSrc_Ex
         oldOffset=$(NewOffset)
-        sleep 2s
+        sleep 1s
         saveMsg ${oldOffset}
         endFlag=$(checkEnd)
     done
-    backRespond 'Bot gets ur request'
-    if [[ $(checkEmpty "Msg_File.tmp") == 0 ]]
+    backRespond 'Bot start to process your request'
+    checkRequest
+    if [[ ${?} == 0 ]]
     then
         createFolder
+        createFolder_Ex
         displayLog "work" "Msg_File.tmp"
-        downFile
+        displayLog_Ex
+        backRespond 'Start to download those file'
+        [[ ${flagArray[0]} = 1 ]] && downFile
+        downFile_Ex
+        backRespond 'Start to transfer the file to Google Drive'
         rcloneMove
         backRespond 'Your request has just been proceeded'
         rm -rf downRespond.tmp
+    else
+        backRespond 'No request was found......'
     fi
     cleanTmp
     oldOffset=$(NewOffset)
@@ -73,15 +115,25 @@ function work(){
 
 function downFile(){
     downArray=($(cat Msg_File.tmp))
-    for ((i=1;${i}<=${#downArray[@]};i++))
+    for ((i=0;${i}<${#downArray[@]};i++))
     do
         Tmethod getFile "file_id=${downArray[${i}]}" > downRespond.tmp
         if [[ $(check_downRespond) == 0 ]]
         then
             oldPath="$(grep -oE 'file_path[^}]+' downRespond.tmp | cut -d'"' -f3)"
-            mv "${oldPath}" downDir/Video/$(grep -oE "file_unique_id[^,]+" downRespond.tmp | cut -d'"' -f3).${oldPath##*.}
+                CompressedFile_Flag=0
+                [[ ! $(echo ${oldPath##*.}| grep -i '7z') == "" ]] && CompressedFile_Flag=`expr ${CompressedFile_Flag} + 1`
+                [[ ! $(echo ${oldPath##*.}| grep -i 'rar') == "" ]] && CompressedFile_Flag=`expr ${CompressedFile_Flag} + 1`
+                [[ ! $(echo ${oldPath##*.}| grep -i 'zip') == "" ]] && CompressedFile_Flag=`expr ${CompressedFile_Flag} + 1`
+                if [[ ${CompressedFile_Flag} == 0 ]]
+                then
+                    mv "${oldPath}" downDir/Video/$(grep -oE "file_unique_id[^,]+" downRespond.tmp | cut -d'"' -f3).${oldPath##*.}
+                else
+                    mv "${oldPath}" downDir/Compressed/$(grep -oE "file_unique_id[^,]+" downRespond.tmp | cut -d'"' -f3).${oldPath##*.}
+                fi
         fi
-        echo "File [${i}]/[${#downArray[@]}] is downloaded"
+        backRespond "File [`expr ${i} + 1`]/[${#downArray[@]}] is downloaded"
+        echo "File [`expr ${i} + 1`]/[${#downArray[@]}] is downloaded"
     done
 }
 
@@ -97,6 +149,7 @@ function backRespond(){
 
 function createFolder(){
     mkdir -p downDir/Video
+    mkdir -p downDir/Compressed
 }
 
 function rcloneMove(){
@@ -104,15 +157,16 @@ function rcloneMove(){
 }
 
 function cleanTmp(){
+    rm -rf *.tmp
     >& Msg.tmp
     >& Msg_File.tmp
-    sleep 2s && clear
+    sleep 2s
 }
 
 function checkEmpty(){
-#$(checkEmpty) return 1 when the file is Empty
-#$(checkEmpty) return 0 when the file is not Empty
-    [[ $(cat ${1}) == "" ]] && echo 1 || echo 0
+#$(checkEmpty) return 0 when the file is Empty
+#$(checkEmpty) return 1 when the file is not Empty
+    [[ $(cat ${1}) == "" ]] && echo 0 || echo 1
 }
 
 function checkEnd(){
